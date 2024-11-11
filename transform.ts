ca2bf73e8853453db81571328c2978bafb5f6eb8
CYoung5233 <117775472+CYoung5233@users.noreply.github.com>
2024-11-11 15:39:30 -0800

    Create transform.ts

---
+++ transform.ts
@@ -0,0 +1,3 @@
+//File to store the transform operations - in typescript
+//Project structure: When using other langauges, make new file of the same name.
+//Ensure code in new file does the ~exact same~ thing as this file
