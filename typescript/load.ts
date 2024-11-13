18cad4e7452103f16f057a1b49f8a6c974c4f3e6
CYoung5233 <cyoung17@uw.edu>
2024-11-13 11:00:45 -0800

    added purpose message

--- typescript/load.ts
+++ typescript/load.ts
@@ -1,3 +1,6 @@
 //File to store the load operations - in typescript
 //Project structure: When using other langauges, make new file of the same name.
 //Ensure code in new file does the ~exact same~ thing as this file
+
+
+//load data into a db
--- typescript/query.ts
+++ typescript/query.ts
@@ -1,3 +1,6 @@
 //File to store the query operations - in typescript
 //Project structure: When using other langauges, make new file of the same name.
 //Ensure code in new file does the ~exact same~ thing as this file
+
+
+//Query from the db set up by load
