7c6fc28f8ab0d43160b19f0c5595169dbbf4e661
Richard <122660178+radams42@users.noreply.github.com>
2024-11-13 12:54:54 -0800

    query.java init WIP

--- java/query.java
+++ java/query.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class query {
         // Load environment variables from .env file
