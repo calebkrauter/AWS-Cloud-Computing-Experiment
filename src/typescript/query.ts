9ea14defe12e57262378b5c6b6d4ee625721a631
CYoung5233 <cyoung17@uw.edu>
2024-11-27 17:31:59 -0800

    Added port

--- src/typescript/query.ts
+++ src/typescript/query.ts
@@ -10,6 +10,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 };
 
 // Function to fetch all data (joined from both Order and Item tables)
