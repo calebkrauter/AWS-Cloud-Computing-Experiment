291a6bf02ea2cfb905fce460700dffc57048af70
radams42 <122660178+radams42@users.noreply.github.com>
2024-12-12 16:43:04 -0800

    WIP (query)

--- src/java_awt/load.java
+++ src/java_awt/load.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Load {
+public class load {
 
     public static void main(String[] args) {
         System.out.println("main");
